e75647838d42c6d4419e8705ec8961bffefd1994
Paulo Andre <pauloandre050619@gmail.com>
2025-05-14 21:51:22 -0300

    Tipos primitivos e Literais Java

---
+++ Argumentos-Java/Argumentos-Exercicio/src/com/paulo/cursojava/aula11/VariaveisPontooFlutuante.java
@@ -0,0 +1,15 @@
+package com.paulo.cursojava.aula11;
+
+public class VariaveisPontooFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 2.90;
+		
+		float valorTomate = 3.95f;
+		
+		System.out.println("Valor da passagem: " + valorPassagem);
+		System.out.println("Valor do tomate: " + valorTomate);
+	}
+
+}
---
+++ Argumentos-Java/Argumentos-Exercicio/src/com/paulo/cursojava/aula11/VariavesChar.java
@@ -0,0 +1,18 @@
+package com.paulo.cursojava.aula11;
+
+public class VariavesChar {
+
+	public static void main(String[] args) {
+		
+//		char o = 'o';
+//		char i = 'i';
+		
+		char o = 111;
+		char i = 105;
+		char interrogacao = 077; //Valor -> ?  
+		
+		System.out.println(""+o+i+interrogacao);
+
+	}
+
+}
